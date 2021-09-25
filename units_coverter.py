18b421cb580c9b0f5e7d82e92bd0017611d25b1f
Xenia Sukhanova <sukhanovaxenia@gmail.com>
2021-09-25 12:10:51 +0300

    changed converter

--- units_coverter.py
+++ units_coverter.py
@@ -4,12 +4,11 @@
 print('Absorbing radiation converter is launched!')
 
 unit = int(input('Print your unit in Gy format: ', ))
-out_format = input('Print to which format do you want to convert (mGy, mcrGy, cGy, rad): ', )
+out_format = input('Print to which format do you want to convert (mGy, mcrGy, rad): ', )
 
 absorb = {
         'mGy' : lambda unit: unit / 1000,
         'mcrGy' : lambda unit: unit / 1000000,
-        'cGy' : lambda unit: unit / 10000,
         'rad' : lambda unit: unit / 10000,
         }
 
